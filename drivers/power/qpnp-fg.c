b8a0295d9be95a8e261ae19720204c6eda99bdb8
Wei Wang <wvw@google.com>
2016-12-12 15:31:49 -0800

    ANDROID: fix uninitialized variables
    Currently we set CONFIG_CC_OPTIMIZE_FOR_SIZE which suppressed the compiler
    warning of unused variables which can lead undefined behavior e.g. memory
    corruption and panic. See https://lkml.org/lkml/2013/3/25/347.
    
    This patch fixes all the uninitilized variables in kernel
    
    Bug: 33353384
    Test: On device
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I0ae1082f447b435d71156d471878ba71aa16c378

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -2521,9 +2521,7 @@ static int estimate_battery_age(struct fg_chip *chip, int *actual_capacity)
 	}
 
 	battery_soc = get_battery_soc_raw(chip) * 100 / FULL_PERCENT_3B;
-	if (rc) {
-		goto error_done;
-	} else if (battery_soc < 25 || battery_soc > 75) {
+	if (battery_soc < 25 || battery_soc > 75) {
 		if (fg_debug_mask & FG_AGING)
 			pr_info("Battery SoC (%d) out of range, aborting\n",
 					(int)battery_soc);
@@ -4475,6 +4473,7 @@ static int fg_batt_profile_init(struct fg_chip *chip)
 	const char *data, *batt_type_str, *old_batt_type;
 	bool tried_again = false, vbat_in_range, profiles_same;
 	u8 reg = 0;
+	old_batt_type = default_batt_type;
 
 wait:
 	fg_stay_awake(&chip->profile_wakeup_source);
@@ -4502,7 +4501,6 @@ wait:
 							fg_batt_type);
 	if (!profile_node) {
 		pr_err("couldn't find profile handle\n");
-		old_batt_type = default_batt_type;
 		rc = -ENODATA;
 		goto fail;
 	}
@@ -5992,7 +5990,6 @@ static int fg_hw_init(struct fg_chip *chip)
 static int fg_setup_memif_offset(struct fg_chip *chip)
 {
 	int rc;
-	u8 dig_major;
 
 	rc = fg_read(chip, chip->revision, chip->mem_base + DIG_MINOR, 4);
 	if (rc) {
@@ -6010,7 +6007,7 @@ static int fg_setup_memif_offset(struct fg_chip *chip)
 		chip->ima_supported = true;
 		break;
 	default:
-		pr_err("Digital Major rev=%d not supported\n", dig_major);
+		pr_err("Digital Major rev=%d not supported\n", chip->revision[DIG_MAJOR]);
 		return -EINVAL;
 	}
 
